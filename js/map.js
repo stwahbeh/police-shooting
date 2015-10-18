88e951ca66ff1648c75dcb8201efc3abc3b63dce
Sam Wahbeh <stwahbeh@gmail.com>
2015-10-17 22:31:12 -0700

    saving files and going home

--- js/map.js
+++ js/map.js
@@ -2,30 +2,39 @@
 var drawMap = function() {
 
   // Create map and set view
- 
+var map = L.map('container').setView([latitude, longitude], zoom)
 
   // Create a tile layer variable using the appropriate url
-
+var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png')
 
   // Add the layer to your map
- 
+layer.addTo(map) 
 
   // Execute your function to get data
- 
+getData(map);
 }
 
 // Function for getting data
-var getData = function() {
+var getData = function(map) {
 
   // Execute an AJAX request to get the data in data/response.js
-
+var data;
+$.ajax({
+     url:'data/response.json',
+     type: "get",
+     success:function(dat) {
+       data = dat
+       // Do something with your data!
+     }, 
+     dataType:"json"
+}) 
 
   // When your request is successful, call your customBuild function
 
 }
 
 // Loop through your data and add the appropriate layers and points
-var customBuild = function() {
+var customBuild = function(map, data) {
 	// Be sure to add each layer to the map
 
 	// Once layers are on the map, add a leaflet controller that shows/hides layers
